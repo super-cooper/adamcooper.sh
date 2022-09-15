3250329a2f0db23eafeb5be88de9e0e4a26fc736
Adam Cooper <adam@adamcooper.sh>
2022-09-14 20:13:14 -0500

    Add keybase.txt

--- src/java/main/sh/adamcooper/Main.kt
+++ src/java/main/sh/adamcooper/Main.kt
@@ -7,6 +7,7 @@ import io.ktor.server.application.log
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.html.respondHtml
+import io.ktor.server.http.content.resource
 import io.ktor.server.http.content.resources
 import io.ktor.server.http.content.static
 import io.ktor.server.plugins.callloging.CallLogging
@@ -50,6 +51,7 @@ fun main() {
                     resources("img")
                 }
             }
+            resource("keybase.txt")
         }
     }.start(wait = true)
 }
