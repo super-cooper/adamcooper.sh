7d226595a24fde4672ad86b832372abf1d399170
Adam Cooper <adam@adamcooper.sh>
2025-04-22 04:07:16 -0400

    Update Wordle dependency

--- src/java/main/sh/adamcooper/apps/wordle/WordleApp.kt
+++ src/java/main/sh/adamcooper/apps/wordle/WordleApp.kt
@@ -11,9 +11,13 @@ import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.Month
+import kotlinx.datetime.daysUntil
 import kotlinx.datetime.plus
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.html.ButtonType
 import kotlinx.html.H3
 import kotlinx.html.HTML
@@ -40,26 +44,32 @@ import kotlinx.html.th
 import kotlinx.html.title
 import kotlinx.html.tr
 import sh.adamcooper.apps.wordle.WordleState.WORDLE_FIRST_DATE
+import sh.adamcooper.infrastructure.LOCAL_TIME_ZONE
 import sh.adamcooper.wordle.Wordle
 import kotlin.time.Duration.Companion.hours
 
 private const val WORDLE_BUFFER_CAPACITY = 1000
 
-/**
- * Manages the local state of Wordle solutions
- */
+/** Manages the local state of Wordle solutions */
 private object WordleState {
     val WORDLE_FIRST_DATE = LocalDate(year = 2021, month = Month.JUNE, dayOfMonth = 19)
     private var lastWordListRefresh = Clock.System.now()
     private val log = KtorSimpleLogger("WordleState")
 
+    val count: Int
+        get() {
+            val today = Clock.System.now().toLocalDateTime(LOCAL_TIME_ZONE).date
+            return WORDLE_FIRST_DATE.daysUntil(today)
+        }
+
     var wordle = Wordle()
         get() {
             if (this.lastWordListRefresh + 1.hours < Clock.System.now()) {
-                field = Wordle().also {
-                    this.log.info("Refreshing Wordle state")
-                    this.lastWordListRefresh = Clock.System.now()
-                }
+                field =
+                    Wordle().also {
+                        this.log.info("Refreshing Wordle state")
+                        this.lastWordListRefresh = Clock.System.now()
+                    }
             }
             return field
         }
@@ -68,9 +78,10 @@ private object WordleState {
     val solutions: Flow<WordleDB.Solution>
         get() = flow {
             val knownSolutions = WordleDB.solutions
-            for (id in WordleState.wordle.count downTo knownSolutions.count()) {
-                WordleState.log.info("Solving new Wordle puzzle $id")
-                val answer = WordleState.wordle.answer(id)
+            for (id in WordleState.count downTo knownSolutions.count()) {
+                val date = WordleState.WORDLE_FIRST_DATE.plus(id, DateTimeUnit.DAY)
+                WordleState.log.info("Solving new Wordle puzzle $id ($date)")
+                val answer = WordleState.wordle.answer(date.toJavaLocalDate())
                 WordleState.log.info("Resolved Wordle answer $answer")
                 val guesses = WordleState.wordle.play(WordleState.wordle.bestWord, answer)
                 emit(WordleDB.saveSolution(id, guesses, answer))
@@ -88,14 +99,16 @@ internal fun H3.wordleDescription() {
     +"the correct answer."
 }
 
-private fun wordleGuessSequence(solution: WordleDB.Solution) = sequenceOf(
-    solution.guess1,
-    solution.guess2,
-    solution.guess3,
-    solution.guess4,
-    solution.guess5,
-    solution.guess6
-).filterNotNull()
+private fun wordleGuessSequence(solution: WordleDB.Solution) =
+    sequenceOf(
+            solution.guess1,
+            solution.guess2,
+            solution.guess3,
+            solution.guess4,
+            solution.guess5,
+            solution.guess6,
+        )
+        .filterNotNull()
 
 private fun TD.wordleFormatted(solution: WordleDB.Solution) {
     for (guess in wordleGuessSequence(solution)) {
@@ -103,8 +116,10 @@ private fun TD.wordleFormatted(solution: WordleDB.Solution) {
         div(classes = "wordleFormattedResultRow") {
             results.zip(guess.asIterable()).forEach { (result, char) ->
                 when (result) {
-                    Wordle.Letter.GREEN -> div(classes = "wordleFormattedResultGreen") { +char.toString() }
-                    Wordle.Letter.YELLOW -> div(classes = "wordleFormattedResultYellow") { +char.toString() }
+                    Wordle.Letter.GREEN ->
+                        div(classes = "wordleFormattedResultGreen") { +char.toString() }
+                    Wordle.Letter.YELLOW ->
+                        div(classes = "wordleFormattedResultYellow") { +char.toString() }
                     else -> div(classes = "wordleFormattedResultBlack") { +char.toString() }
                 }
             }
@@ -112,22 +127,26 @@ private fun TD.wordleFormatted(solution: WordleDB.Solution) {
     }
 }
 
-private fun wordleCopyText(solution: WordleDB.Solution): String = buildString {
-    val guesses = wordleGuessSequence(solution)
-    val count = solution.guess6?.let { if (it != solution.answer) "X" else null } ?: guesses.count().toString()
-    appendLine("Wordle ${solution.id} $count/6")
-    appendLine()
-    for (guess in guesses) {
-        for (letter in WordleState.wordle.getResultOfGuess(guess, solution.answer)) {
-            when (letter) {
-                Wordle.Letter.GREEN -> append("🟩")
-                Wordle.Letter.YELLOW -> append("🟨")
-                Wordle.Letter.BLACK -> append("⬛")
+private fun wordleCopyText(solution: WordleDB.Solution): String =
+    buildString {
+            val guesses = wordleGuessSequence(solution)
+            val count =
+                solution.guess6?.let { if (it != solution.answer) "X" else null }
+                    ?: guesses.count().toString()
+            appendLine("Wordle ${solution.id} $count/6")
+            appendLine()
+            for (guess in guesses) {
+                for (letter in WordleState.wordle.getResultOfGuess(guess, solution.answer)) {
+                    when (letter) {
+                        Wordle.Letter.GREEN -> append("🟩")
+                        Wordle.Letter.YELLOW -> append("🟨")
+                        Wordle.Letter.BLACK -> append("⬛")
+                    }
+                }
+                appendLine()
             }
         }
-        appendLine()
-    }
-}.trim()
+        .trim()
 
 private fun TABLE.wordleDisplay(solution: WordleDB.Solution) {
     val wordleNumber = solution.id.value
@@ -154,11 +173,12 @@ fun HTML.wordleApp() {
         // Fonts
         link(
             href = "https://fonts.googleapis.com/css?family=Roboto&display=swap",
-            rel = LinkHeader.Rel.Stylesheet
+            rel = LinkHeader.Rel.Stylesheet,
         )
         link(
-            href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
-            rel = LinkRel.stylesheet
+            href =
+                "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+            rel = LinkRel.stylesheet,
         )
     }
 
@@ -175,12 +195,8 @@ fun HTML.wordleApp() {
         }
 
         div(classes = "app") {
-            h1 {
-                +"Wordle Solver"
-            }
-            div(classes = "appIntroduction") {
-                h3 { this.wordleDescription() }
-            }
+            h1 { +"Wordle Solver" }
+            div(classes = "appIntroduction") { h3 { this.wordleDescription() } }
             div(classes = "wordleTableContainer") {
                 table(classes = "wordleTable") {
                     tr {
@@ -191,8 +207,11 @@ fun HTML.wordleApp() {
                         th { +"Copy" }
                     }
                     runBlocking {
-                        WordleState.solutions.buffer(WORDLE_BUFFER_CAPACITY).toList()
-                            .sortedByDescending(WordleDB.Solution::id).forEach(this@table::wordleDisplay)
+                        WordleState.solutions
+                            .buffer(WORDLE_BUFFER_CAPACITY)
+                            .toList()
+                            .sortedByDescending(WordleDB.Solution::id)
+                            .forEach(this@table::wordleDisplay)
                     }
                 }
             }
