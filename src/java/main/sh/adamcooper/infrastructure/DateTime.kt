0c7eb83149ea18c518c98abd81daab95539bea11
Adam Cooper <adam@adamcooper.sh>
2025-04-22 04:06:42 -0400

    Change timezone to New York

--- src/java/main/sh/adamcooper/infrastructure/DateTime.kt
+++ src/java/main/sh/adamcooper/infrastructure/DateTime.kt
@@ -2,4 +2,4 @@ package sh.adamcooper.infrastructure
 
 import kotlinx.datetime.TimeZone
 
-val LOCAL_TIME_ZONE = TimeZone.of("America/Chicago")
+val LOCAL_TIME_ZONE = TimeZone.of("America/New_York")
