1a36925d331429925a564fd805b59f31d25b0bc5
Adam Cooper <adam@adamcooper.sh>
2025-05-17 19:11:10 -0400

    Update deprecated static routes

--- src/java/main/sh/adamcooper/Main.kt
+++ src/java/main/sh/adamcooper/Main.kt
@@ -6,9 +6,7 @@ import io.ktor.server.application.log
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.html.respondHtml
-import io.ktor.server.http.content.resource
-import io.ktor.server.http.content.resources
-import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.routing.application
 import io.ktor.server.routing.get
@@ -25,47 +23,30 @@ import sh.adamcooper.static.opinions
 import sh.adamcooper.static.projects
 import sh.adamcooper.static.respondCss
 
-/**
- * Definition for the server
- */
+/** Definition for the server */
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
-        install(CallLogging)
-        routing {
-            trace { this.application.log.trace(it.buildText()) }
-            get("/") {
-                this.call.respondHtml(HttpStatusCode.OK, HTML::index)
-            }
-            get("/about") {
-                this.call.respondHtml(HttpStatusCode.OK, HTML::about)
-            }
-            route("/projects") {
-                get {
-                    this.call.respondHtml(HttpStatusCode.OK, HTML::projects)
+            install(CallLogging)
+            routing {
+                trace { this.application.log.trace(it.buildText()) }
+                get("/") { this.call.respondHtml(HttpStatusCode.OK, HTML::index) }
+                get("/about") { this.call.respondHtml(HttpStatusCode.OK, HTML::about) }
+                route("/projects") {
+                    get { this.call.respondHtml(HttpStatusCode.OK, HTML::projects) }
+                    get("/wordle") { this.call.respondHtml(HttpStatusCode.OK, HTML::wordleApp) }
                 }
-                get("/wordle") {
-                    this.call.respondHtml(HttpStatusCode.OK, HTML::wordleApp)
+                get("/opinions") { this.call.respondHtml(HttpStatusCode.OK, HTML::opinions) }
+                get("/contact") { this.call.respondHtml(HttpStatusCode.OK, HTML::contact) }
+                route("/static") {
+                    get("style.css") { this.call.respondCss(CssBuilder::globalStyle) }
+                    staticResources("img", "img")
+                    staticResources("scripts", "scripts") {
+                        staticResources("adamcooper-sh.js", "scripts")
+                        staticResources("adamcooper-sh.js.map", "scripts")
+                    }
                 }
+                staticResources("", "") { staticResources("keybase.txt", "") }
             }
-            get("/opinions") {
-                this.call.respondHtml(HttpStatusCode.OK, HTML::opinions)
-            }
-            get("/contact") {
-                this.call.respondHtml(HttpStatusCode.OK, HTML::contact)
-            }
-            static("/static") {
-                get("style.css") {
-                    this.call.respondCss(CssBuilder::globalStyle)
-                }
-                static("img") {
-                    resources("img")
-                }
-                static("scripts") {
-                    resource("adamcooper-sh.js")
-                    resource("adamcooper-sh.js.map")
-                }
-            }
-            resource("keybase.txt")
         }
-    }.start(wait = true)
+        .start(wait = true)
 }
