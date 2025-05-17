70056813d55058cfd1396815ace5ab60eba25a89
Adam Cooper <adam@adamcooper.sh>
2025-05-17 16:19:40 -0400

    Update build infra

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
-val kotlinVersion: String by project
-val ktorVersion: String by project
-val logbackVersion: String by project
-val exposedVersion: String by project
-val kotlinxCSSVersion = "1.0.0-pre.407"
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-plugins {
-    kotlin("multiplatform") version "1.7.20"
-    application
-}
+plugins { alias(libs.plugins.kotlin.multiplatform) }
 
 group = "sh.adamcooper"
+
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -18,24 +13,15 @@ repositories {
 }
 
 kotlin {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
     jvm("java") {
-        compilations.all {
-            kotlinOptions.jvmTarget = "16"
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
+        compilerOptions { jvmTarget = JvmTarget.JVM_23 }
+        testRuns["test"].executionTask.configure { useJUnitPlatform() }
+        binaries { executable { mainClass.set("sh.adamcooper.MainKt") } }
     }
     js(IR) {
         binaries.executable()
-        browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled = true
-                }
-            }
-        }
+        browser { commonWebpackConfig { cssSupport { enabled = true } } }
     }
     sourceSets {
         val commonMain by getting {
@@ -44,49 +30,28 @@ kotlin {
         }
         val commonTest by getting {
             kotlin.srcDir("src/common/test")
-            dependencies {
-                implementation(kotlin("test"))
-            }
+            dependencies { implementation(kotlin("test")) }
         }
         val javaMain by getting {
             kotlin.srcDir("src/java/main")
             resources.srcDir("src/java/main/resources")
             dependencies {
-                implementation("io.ktor:ktor-server-cio:$ktorVersion")
-                implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.412")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-                implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
-                implementation("ch.qos.logback:logback-classic:$logbackVersion")
-
-                implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-                implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-                implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-                implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-                implementation("mysql:mysql-connector-java:8.0.31")
-
-                implementation("sh.adamcooper:wordle:1.0-SNAPSHOT")
+                implementation(libs.bundles.apps)
+                implementation(libs.bundles.db)
+                implementation(libs.bundles.logging)
+                implementation(libs.bundles.kotlin.datetime)
+                implementation(libs.bundles.kotlin.web)
+                implementation(libs.bundles.ktor.server)
             }
         }
-        val javaTest by getting {
-            kotlin.srcDir("src/java/test")
-        }
+        val javaTest by getting { kotlin.srcDir("src/java/test") }
         val jsMain by getting {
             resources.srcDir("src/js/main/resources")
             kotlin.srcDir("src/js/main")
-            dependencies {
-                implementation("org.jetbrains.kotlin-wrappers:kotlin-css:$kotlinxCSSVersion")
-            }
+            dependencies { implementation(libs.kotlin.css) }
         }
-        val jsTest by getting {
-            kotlin.srcDir("src/js/test")
-        }
+        val jsTest by getting { kotlin.srcDir("src/js/test") }
     }
-}
-
-application {
-    mainClass.set("sh.adamcooper.MainKt")
 }
 
 tasks.named<Copy>("javaProcessResources") {
@@ -95,15 +60,11 @@ tasks.named<Copy>("javaProcessResources") {
 }
 
 tasks.named<Jar>("javaJar") {
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    manifest {
-        attributes["Main-Class"] = application.mainClass
-    }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
+    dependsOn("jsProductionExecutableCompileSync")
+    from("${layout.buildDirectory}/js/packages/adamcooper-sh/kotlin") { into("static/js") }
 }
 
-tasks.named<JavaExec>("run") {
+tasks.named<JavaExec>("runJava") {
     dependsOn(tasks.named<Jar>("javaJar"))
     classpath(tasks.named<Jar>("javaJar"))
 }
